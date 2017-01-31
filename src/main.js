a6aa3f5761b4c04c87f1a52495d4f6f8b8dfdf3a
Aman Sachan <asach@seas.upenn.edu>
2017-01-30 23:22:41 -0500

    wind working with some sort of positional persistence but rotation is spring based

--- src/main.js
+++ src/main.js
@@ -6,13 +6,14 @@ import Framework from './framework'
 
 var levelOfDetail = 50;
 var feathers = new THREE.Object3D();
+var splineObject = new THREE.Line();
 
 var guiParameters = {
     windStrength: 3.0, //0 to 10
-    windDirectionX: 0.0, //0 to 1
-    windDirectionY: 0.0, //0 to 1
-    windDirectionZ: 1.0, //0 to 1
-    curvature: 0.0, //increase curvature of spline 1 to 10
+    windDirectionX: 0.1, //0 to 1
+    windDirectionY: 0.1, //0 to 1
+    windDirectionZ: 0.1, //0 to 1
+    curvature: 0.1, //increase curvature of spline 1 to 10
     Layer1ColorR: 0.0902,
     Layer1ColorG: 0.1961,
     Layer1ColorB: 0.5411,
@@ -22,11 +23,11 @@ var guiParameters = {
     Layer3ColorR: 0.7,
     Layer3ColorG: 0.3,
     Layer3ColorB: 0.3,
-    featherDistribution: 0.0, //clumping factor from 0 to 1
+    featherDistribution: 1.0, //clumping factor from 0 to 1
     featherSize: 1.0, //
     featherOrientation: 1.0, //increase curvature of individual feathers 1 to 10
     flappingSpeed: 1.0, //0 to 10
-    flappingMotion: 0.0 //change orientation of entire wing 0 to 1
+    flappingMotion: 1.0 //change orientation of entire wing 0 to 1
 }
 
 var feather_Material = new THREE.ShaderMaterial({
@@ -45,6 +46,10 @@ var feather_Material = new THREE.ShaderMaterial({
 function dynamicLayers()
 {
 }
+
+var featherOriginalX = [];
+var featherOriginalY = [];
+var featherOriginalZ = [];
 
 // called after the scene loads
 function onLoad(framework) {
@@ -90,7 +95,7 @@ function onLoad(framework) {
    var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
 
    // Create the final object to add to the scene
-   var splineObject = new THREE.Line( splineGeom, material );
+   splineObject = new THREE.Line( splineGeom, material );
    scene.add(splineObject);
 
     // load a simple obj mesh
@@ -118,6 +123,13 @@ function onLoad(framework) {
         }
     });
 
+    for(var i=0; i<levelOfDetail; i++)
+    {
+      featherOriginalX.push(0);
+      featherOriginalY.push(0);
+      featherOriginalZ.push(0);
+    }
+
     var radianX = -90 * (Math.PI/180);
     feathers.rotateX( radianX );
     splineObject.rotateX( radianX );
@@ -133,83 +145,131 @@ function onLoad(framework) {
 
     // edit params and listen to changes like this
     // more information here: https://workshop.chromeexperiments.com/examples/gui/#1--Basic-Usage
-    gui.add(camera, 'fov', 0, 180).onChange(function(newVal) {
-        camera.updateProjectionMatrix();
-    });
-    gui.add(guiParameters, 'windStrength', 0, 10).onChange(function(newVal)
-    {
-      guiParameters.windStrength = newVal;
-    });
-    gui.add(guiParameters, 'windDirectionX', 0, 1).onChange(function(newVal)
-    {
-      guiParameters.windDirectionX = newVal;
-    });
-    gui.add(guiParameters, 'windDirectionY', 0, 1).onChange(function(newVal)
-    {
-      guiParameters.windDirectionY = newVal;
-    });
-    gui.add(guiParameters, 'windDirectionZ', 0, 1).onChange(function(newVal)
-    {
-      guiParameters.windDirectionZ = newVal;
-    });
-    gui.add(guiParameters, 'curvature', 1, 10).onChange(function(newVal)
-    {
-      guiParameters.curvature = newVal;
-    });
-    gui.add(guiParameters, 'Layer1ColorR', 0, 1).onChange(function(newVal)
+    var f1 = gui.addFolder('Colors');
+    f1.add(guiParameters, 'Layer1ColorR', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.Layer1ColorR = newVal;
     });
-    gui.add(guiParameters, 'Layer1ColorG', 0, 1).onChange(function(newVal)
+    f1.add(guiParameters, 'Layer1ColorG', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.Layer1ColorG = newVal;
     });
-    gui.add(guiParameters, 'Layer1ColorB', 0, 1).onChange(function(newVal)
+    f1.add(guiParameters, 'Layer1ColorB', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.Layer1ColorB = newVal;
     });
-    gui.add(guiParameters, 'featherDistribution', 0, 1).onChange(function(newVal)
+
+    var f2 = gui.addFolder('Feathers');
+    f2.add(guiParameters, 'featherDistribution', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.featherDistribution = newVal;
     });
-    gui.add(guiParameters, 'featherSize', 0, 1).onChange(function(newVal)
+    f2.add(guiParameters, 'featherSize', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.featherSize = newVal;
     });
-    gui.add(guiParameters, 'featherOrientation', 0, 1).onChange(function(newVal)
+    f2.add(guiParameters, 'featherOrientation', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.featherOrientation = newVal;
     });
-    gui.add(guiParameters, 'flappingSpeed', 0, 1).onChange(function(newVal)
+
+    var f3 = gui.addFolder('Flapping');
+    f3.add(guiParameters, 'flappingSpeed', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.flappingSpeed = newVal;
     });
-    gui.add(guiParameters, 'flappingMotion', 0, 1).onChange(function(newVal)
+    f3.add(guiParameters, 'flappingMotion', 0.0, 1.0).onChange(function(newVal)
     {
       guiParameters.flappingMotion = newVal;
     });
+
+    var f4 = gui.addFolder('Wind');
+    f4.add(guiParameters, 'windStrength', 0.0, 10.0).onChange(function(newVal)
+    {
+      guiParameters.windStrength = newVal;
+    });
+    f4.add(guiParameters, 'windDirectionX', 0.0, 1.0).onChange(function(newVal)
+    {
+      guiParameters.windDirectionX = newVal;
+    });
+    f4.add(guiParameters, 'windDirectionY', 0.0, 1.0).onChange(function(newVal)
+    {
+      guiParameters.windDirectionY = newVal;
+    });
+    f4.add(guiParameters, 'windDirectionZ', 0.0, 1.0).onChange(function(newVal)
+    {
+      guiParameters.windDirectionZ = newVal;
+    });
+
+    var f5 = gui.addFolder('Camera');
+    f5.add(camera, 'fov', 0.0, 180.0).onChange(function(newVal) {
+        camera.updateProjectionMatrix();
+    });
+
+    var f6 = gui.addFolder('Wing Shape');
+    f6.add(guiParameters, 'curvature', 1.0, 10.0).onChange(function(newVal)
+    {
+      guiParameters.curvature = newVal;
+    });
 }
 
-// function noisehash(var t)
-// {
-//   t  = fract( t*0.3183099+0.1 ) * 17.0;
-//   return fract( t );
-// }
+function noisehash(x)
+{
+  // t = t*0.3183099+0.1 * 17.0;
+  // t /= 1000.0;
+  // return t;
+
+  // x = (x<<13 ^ x);
+  // return (1.0 - (x * (x * x * 15731 +)))
+}
+
+function noisehash2(x,y)
+{
+  return (Math.cos(x) + Math.sin(y));
+}
 
 // called on frame updates
 function onUpdate(framework)
 {
+  var date = new Date();
+  var feather = framework.scene.getObjectByName("feather");
+  if (feather !== undefined)
+  {
+      // Simply flap wing
+
+      //feather.rotateZ(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);
+      for(var i=0; i<levelOfDetail; i++)
+      {
+        //feathers.children[i].rotateZ(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);
+
+        feathers.children[i].rotateX(-featherOriginalX[i]);
+        feathers.children[i].rotateY(-featherOriginalY[i]);
+        feathers.children[i].rotateZ(-featherOriginalZ[i]);
+
+        var rx = (Math.sin(date.getTime() / 100) * 2 * Math.PI / 180) *
+                   (guiParameters.windDirectionX * guiParameters.windStrength)
+                    * Math.random();
+
+        var ry =(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180) *
+                  (guiParameters.windDirectionY * guiParameters.windStrength)
+                  * Math.random();
+
+        var rz=(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180) *
+                  (guiParameters.windDirectionZ * guiParameters.windStrength)
+                  * Math.random();
+
+        feathers.children[i].rotateX(rx);
+        feathers.children[i].rotateY(ry);
+        feathers.children[i].rotateZ(rz);
+
+        featherOriginalX[i] = rx;
+        featherOriginalY[i] = ry;
+        featherOriginalZ[i] = rz;
+      }
+  }
     var date = new Date();
+    splineObject.rotateY(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);
     feathers.rotateY(Math.sin(date.getTime() / 100) * 2 * Math.PI / 180);
-    for(var i=0; i<levelOfDetail; i++)
-    {
-      feathers.children[i].rotateX((Math.sin(date.getTime() / 100) * 2 * Math.PI / 180) *
-                                   (guiParameters.windDirectionX * guiParameters.windStrength));
-      feathers.children[i].rotateY((Math.sin(date.getTime() / 100) * 2 * Math.PI / 180) *
-                                   (guiParameters.windDirectionY * guiParameters.windStrength));
-      feathers.children[i].rotateZ((Math.sin(date.getTime() / 100) * 2 * Math.PI / 180) *
-                                   (guiParameters.windDirectionZ * guiParameters.windStrength));
-    }
 
     feather_Material.uniforms.feathercolor.value = new THREE.Vector3( guiParameters.Layer1ColorR,
                                                                       guiParameters.Layer1ColorG,
